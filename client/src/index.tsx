54730e485ce52675e656d43a684c708a5a06ac34
muhdamean <muhdamean@gmail.com>
2021-11-24 13:40:14 +0100

    Section 7

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,16 +5,26 @@ import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from 'react-router-dom';
 import {createBrowserHistory} from "history"
-import { StoreProvider } from './app/context/StoreContext';
+//import { StoreProvider } from './app/context/StoreContext';
+//import { configureStore } from './app/store/configureStore';
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
+
+//const store=configureStore();
+//console.log(store.getState());
 
 export const history= createBrowserHistory();
+
+//store.dispatch(fetchProductsAsync())
 
 ReactDOM.render(
   <React.StrictMode>
     <Router history={history}>
-      <StoreProvider>
-        <App />
-      </StoreProvider>
+      {/* <StoreProvider> */}
+          <Provider store={store}>
+              <App />
+          </Provider>
+      {/* </StoreProvider> */}
     </Router> 
   </React.StrictMode>,
   document.getElementById('root')
