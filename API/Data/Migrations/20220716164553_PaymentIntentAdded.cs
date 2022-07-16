5cfdfda6c2ecb06e43b1f4dad25bff33e14974ee
muhdamean <muhdamean@gmail.com>
2022-07-16 17:47:40 +0100

    ADD: Add PaymentIntent for Stripe

---
+++ API/Data/Migrations/20220716164553_PaymentIntentAdded.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class PaymentIntentAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientSecret",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "f5b3f368-1444-4ce7-ab99-2b8a2e5d4874");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "b2fc3322-25a0-430e-b82e-f88f25c21c43");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ClientSecret",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Baskets");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "8a5633aa-40ca-4cfe-bef2-0bda918392bd");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "ed01bd80-6c3e-42b5-9ac1-582c15e0001b");
+        }
+    }
+}
