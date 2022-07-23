d072607ef4320a5916903385071b0661ec3994d1
muhdamean <muhdamean@gmail.com>
2022-07-23 16:32:53 +0100

    Publish to heroku

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,17 +14,20 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System;
 
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            WebHostEnvironment = environment;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebHostEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -60,10 +63,40 @@ namespace API
                     }
                 });
             });
-            services.AddDbContext<StoreContext>(opt=>
+            services.AddDbContext<StoreContext>(options =>
             {
-                opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
-                //opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")); //DefaultConnection": "Data Source=store.db"
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                
+                string connStr;
+                
+                if (env == "Development")
+                {
+                    // Use connection string from file.
+                    connStr = Configuration.GetConnectionString("DefaultConnection");
+                    //opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")); //DefaultConnection": "Data Source=store.db"
+                }
+                else
+                {
+                    // Use connection string provided at runtime by Heroku.
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                    // Parse connection URL to connection string for Npgsql
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
+                }
+
+                // Whether the connection string came from the local development configuration file
+                // or from the environment variable from Heroku, use it to set up your DbContext.
+                options.UseNpgsql(connStr);
             });
             services.AddCors();
             services.AddIdentityCore<User>(opt=>
